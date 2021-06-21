d82f515d9761a3e80a4ed39a3932d48303a566a2
이예찬 <firecare@naver.com>
2021-06-21 18:00:35 +0900

    ddd

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -9,7 +9,9 @@ public class MemberApp {
 
     //psvm
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,20 +1,17 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
 
-    @Test
-    void join(){
-        //given
-        Member member = new Member(1L, "memberA", Grade.VIP);
-        //when
-        memberService.join(member);
-        Member findMember = memberService.findMember(1L);
-        //ther
-        Assertions.assertThat(member).isEqualTo(findMember);
+    MemberService memberService;
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
     }
 }
