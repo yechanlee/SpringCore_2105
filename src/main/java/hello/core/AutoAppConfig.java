c3863293f3c820733873f325cfc0d0d604c0dfcb
이예찬 <firecare@naver.com>
2021-07-13 08:27:53 +0900

    0712

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,6 +1,9 @@
 package hello.core;
 
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -13,4 +16,10 @@ import org.springframework.context.annotation.FilterType;
 
 public class AutoAppConfig {
 
+    //수동빈이 이긴다 오버빈을
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+
+    }
 }
